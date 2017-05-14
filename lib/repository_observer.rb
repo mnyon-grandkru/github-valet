1d1a5b66fabc94f23158298d5bc67664eeeb86a0
Mark Nyon <mark@grandkru.com>
2017-05-14 14:51:23 +0000

    RepositoryObserver.readme_md_exists_for?
    - .ruby-gemset - Added Ruby gemset
    
    - .ruby-version - Added Ruby version
    
    - lib/repository_observer.rb - require repository_observer/version
      and Octokit. Added class method readme_md_exists_for?
    
    - repository_observer.gemspec - removed ruboty (could not get it
      to work) and added octokit
    
    - spec/lib/repository_observer_spec.rb - added successful spec
      plus spec for when repository does not exist
    
    Updates #2

--- lib/repository_observer.rb
+++ lib/repository_observer.rb
@@ -1,5 +1,11 @@
-require "repository_observer/version"
-
 module RepositoryObserver
-  # Your code goes here...
+  require 'repository_observer/version'
+  require 'octokit'
+  
+  def self.readme_md_exists_for?(repository) 
+    Octokit.readme(repository).is_a?(Sawyer::Resource)
+  rescue StandardError => error
+    p "Error fetching repo: #{error}"
+    false
+  end
 end
--- spec/lib/repository_observer_spec.rb
+++ spec/lib/repository_observer_spec.rb
@@ -4,8 +4,15 @@ describe RepositoryObserver do
   describe '.readme_md_exists_for?(repository)' do
     describe 'on success' do
       it 'returns true if the Github repository has a README file' do
-        github_repo_url = "git@github.com:trystant/respository-observer.git"
-        expect(RepositoryObserver.readme_md_exists_for?(github_repo_url)).to be_true
+        github_repo_url = "trystant/respository-observer"
+        expect(RepositoryObserver.readme_md_exists_for?(github_repo_url)).to be_truthy
+      end
+    end
+    
+    describe 'on failure' do
+      it 'returns false if the Github repository does not exist' do
+        github_repo_url = 'blah/wrong-repo'
+        expect(RepositoryObserver.readme_md_exists_for?(github_repo_url)).to be_falsey
       end
     end
   end
